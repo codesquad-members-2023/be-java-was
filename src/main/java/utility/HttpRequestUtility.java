e62872578f3e3116252062264c0ecd5870755f51
KOKEONHO <rhrjsgh97@gmail.com>
2023-04-03 14:33:53 +0900

    Feat: HttpRequestUtility.java 생성 및 RequestHandler.java에서 추출한 코드 작성
    기존 RequestHandler.java에서 동작되던 getUrl()의 재사용을 위해 HttpRequestUtility.java를 생성하고 추출한 getUrl() 코드 작성

--- src/main/java/utility/HttpRequestUtility.java
+++ src/main/java/utility/HttpRequestUtility.java
@@ -1,7 +1,17 @@
 package utility;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class HttpRequestUtility {
 
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestUtility.class);
 
+    public static String getUrl(String firstLine) {
+        String[] splittedRequestHeader = firstLine.split(" ");
+        String path = splittedRequestHeader[1];
+        logger.debug("request path: {}", path);
+        return path;
+    }
 
 }
