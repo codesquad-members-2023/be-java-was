4ebe2c1dba722b8807c0b3fda55792fa3d1a7557
saehim <saeilee.vxv@gmail.com>
2023-04-03 13:18:18 +0900

    Update : getPath 예외처리 추가
    - '/' 로 시작하지 않으면 예외발생
    - 요청이 잘못들어오는 일이 있을까? 해두고보니 필요없을지도 모르겠다.

--- src/main/java/util/RequestParser.java
+++ src/main/java/util/RequestParser.java
@@ -1,7 +1,14 @@
 package util;
 
 public class RequestParser {
+    private final String PATH_REG = "/[.]*";
     public String getPath(String line) {
-        return line.split(" ")[1];
+        String path = line.split(" ")[1];
+
+        if (!path.matches(PATH_REG)) {
+            throw new IllegalArgumentException("잘못된 경로입니다.");
+        }
+
+        return path;
     }
 }
