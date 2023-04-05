d808d5bfa850390f4ca68b283385d1c492d54a97
saehim <saeilee.vxv@gmail.com>
2023-04-05 15:27:03 +0900

    Feat (requesthandler) : `request`, `response` 생성, `FrontController` 호출
    - 기존 기능을 많이 축소하려고 노력해보았다.

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -3,20 +3,17 @@ package webserver;
 import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.Map;
 
-import model.User;
+import controller.FrontController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import service.UserService;
-import util.RequestParser;
+import webserver.protocol.HttpRequest;
+import webserver.protocol.HttpResponse;
 
 public class RequestHandler implements Runnable {
-    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
 
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private Socket connection;
-    private RequestParser parser;
-    private UserService userService;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -25,61 +22,29 @@ public class RequestHandler implements Runnable {
     public void run() {
         logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-        parser = new RequestParser();
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            DataOutputStream dos = new DataOutputStream(out);
-
             BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            StringBuilder sb = new StringBuilder();
             String requestLine = br.readLine();
 
-            String path = parser.getPath(requestLine);
-            String method = parser.getMethod(requestLine);
-
-            if (path.equals("/user/create") && method.equals("GET")) {
-                userService = new UserService();
-                Map<String, String> param = parser.getQueryParammeter(requestLine);
-                userService.join(param);
-
-                response302Header(dos,"/index.html");
+            String header;
+            while (!((header = br.readLine()).equals(""))) {
+                sb.append(header).append("\n");
             }
+            logger.info("requestLine = {}", requestLine);
+            logger.info("----- header start -----\n" +
+                    sb.toString() +
+                    "\n----- header end -----");
 
+            HttpRequest httpRequest = new HttpRequest(requestLine);
+            HttpResponse httpResponse = new HttpResponse(new DataOutputStream(out));
 
-            byte[] body = getClass().getResourceAsStream("/templates" + path).readAllBytes();
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            FrontController controller = new FrontController();
+            controller.run(httpRequest, httpResponse);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void response302Header(DataOutputStream dos, String path) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Location: "+path+"\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
-
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
 }
