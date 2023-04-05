b3878c8a13353348af4730f985b418bafc730b88
saehim <saeilee.vxv@gmail.com>
2023-04-05 21:10:31 +0900

    Refactor : 포함 관계를 명확하게 보여주려 지역변수 controller를 멤버변수로 변경

--- src/main/java/controller/FrontController.java
+++ src/main/java/controller/FrontController.java
@@ -6,12 +6,12 @@ import webserver.protocol.HttpRequest;
 import webserver.protocol.HttpResponse;
 
 public class FrontController {
+    private Controller controller;
     private final String USER_URL = "/user/";
 
     // PATH를 받아 작업을 담당하는 컨트롤러 생성자를 호출하여 작업을 시키는 역할을 한다.
     public void run(HttpRequest httpRequest, HttpResponse httpResponse) {
         String path = httpRequest.getPath();
-        Controller controller;
 
         if (path.startsWith(USER_URL)) {     // user 관련 요청을 받는 경우
             controller = AppConfig.getUserController();  // 의존성을 낮추기 위해 추상 타입에 의존하는 것이 좋다.
