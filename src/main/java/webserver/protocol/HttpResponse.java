7107ffb304a6c3bbd7d0deda6f77b16612099698
saehim <saeilee.vxv@gmail.com>
2023-04-05 15:25:34 +0900

    Feat (HttpResponse) : 1차 구현
    - 메서드 시그니처 수정 / 추후 Header 세팅이 편하도록 구조화하려고 노력했다.
    - 리팩토링 시에 httpResponse 구조와 헤더를 세팅하는 기능을 좀 나눠볼 예장

---
+++ src/main/java/webserver/protocol/HttpResponse.java
@@ -0,0 +1,79 @@
+package webserver.protocol;
+
+import webserver.RequestHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.Map;
+
+public class HttpResponse {
+    private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
+    private DataOutputStream dos;
+    private String responseLine;
+    private Map<String, String> headers;
+    private byte[] body;
+
+    public HttpResponse(DataOutputStream dos) {
+        this.dos = dos;
+    }
+
+    public HttpResponse forward(String path) throws IOException {
+        body = getClass().getResourceAsStream("/templates" + path).readAllBytes();
+        try {
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes("Content-Type: text/html; charset=UTF-8 \r\n");
+            dos.writeBytes("Content-Length: " + body.length + "\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+        return this;
+    }
+
+    public HttpResponse redirect(String path) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: "+path+"\r\n");
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+        return this;
+    }
+
+    public HttpResponse forward(String path, StyleType type) throws IOException {
+        try {
+            body = Files.readAllBytes(new File("src/main/resources/static"+path).toPath());
+            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+            dos.writeBytes(String.format("Content-Length: %d \r\n", body.length));
+            dos.writeBytes(String.format("Content-Type: %s;charset=utf-8\r\n", type.getValue()));
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+        return this;
+    }
+
+    private HttpResponse setBody() {
+        try {
+            dos.writeBytes("\r\n");
+            dos.write(body, 0, body.length);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+        return this;
+    }
+
+
+    public void response() {
+        try {
+            if (body.length > 0) setBody(); // 응답 하기 전, body가 있는 경우 자동으로 body를 뒤에 붙인다.
+
+            dos.writeBytes("\r\n");
+            dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
