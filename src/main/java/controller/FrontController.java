4c72820cbeeff4b35e1c4a3f3b5e8a0601c27099
saehim <saeilee.vxv@gmail.com>
2023-04-05 14:10:21 +0900

    Feat (controller) : `FrontController` 초안 구현
    요청 url을 1차로 분석하여 작업을 담당하는 컨트롤러에게 역할을 준다.

---
+++ src/main/java/controller/FrontController.java
@@ -0,0 +1,39 @@
+package controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import webserver.protocol.HttpRequest;
+import webserver.protocol.HttpResponse;
+import webserver.protocol.StyleType;
+
+import java.io.IOException;
+
+public class FrontController {
+    private Logger logger = LoggerFactory.getLogger(FrontController.class);
+
+    // PATH를 받아 작업을 담당하는 컨트롤러 생성자를 호출하여 작업을 시키는 역할을 한다.
+    public void run(HttpRequest httpRequest, HttpResponse httpResponse) {
+        try {
+            Controller controller;
+            String path = httpRequest.getPath();
+
+            if (StyleType.anyMatchStyle(path).isPresent()) {   // style 요청을 받은 경우
+                logger.info("path = {}", httpRequest.getPath());
+                controller = new StyleController();
+                controller.run(httpRequest, httpResponse);
+            }
+
+            // user 관련 요청을 받은 경우
+            if (path.startsWith("/user")) {
+                controller = new UserController();
+                controller.run(httpRequest, httpResponse);
+            }
+
+            // 기본값 컨트롤러
+            controller = new ViewController();
+            controller.run(httpRequest, httpResponse);
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
