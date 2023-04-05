b0ae8fbd86af44126c788abb9a48aa18f2aac44f
birdieHyun <flaehdan@gmail.com>
2023-04-05 20:15:39 +0900

    Refactor : 불필요한 코드 제거

--- src/main/java/util/HttpStatus.java
+++ src/main/java/util/HttpStatus.java
@@ -1,26 +1,20 @@
 package util;
 
 public enum HttpStatus {
-    OK(200, HttpStatusSeries.SUCCESS, "OK"),
-    MOVED_TEMPORARILY(302, HttpStatusSeries.REDIRECT, "Moved Temporarily");
+    OK(200, "OK"),
+    MOVED_TEMPORARILY(302, "Moved Temporarily");
 
 
     private int value;
-    private HttpStatusSeries httpStatusSeries;
     private String reason;
 
-    HttpStatus(int value, HttpStatusSeries httpStatusSeries, String reason) {
+    HttpStatus(int value, String reason) {
         this.value = value;
-        this.httpStatusSeries = httpStatusSeries;
         this.reason = reason;
     }
 
     public int getValue() {
         return value;
-    }
-
-    public HttpStatusSeries getHttpStatusSeries() {
-        return httpStatusSeries;
     }
 
     public String getReason() {
