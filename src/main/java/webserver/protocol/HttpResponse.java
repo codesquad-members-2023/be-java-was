95bba10c2f00b77b4531fb94bb751f75c7acd0aa
saehim <saeilee.vxv@gmail.com>
2023-04-05 21:09:50 +0900

    Refactor (httpResponse) : 각 요소 멤버 변수로 저장
    - 코드 가독성을 위해 코드 순서 변경
    - dos에 작성하는 역할과 변수를 설정하는 역할에 대해 메서드 분리
    - `response()` 메서드를 통해 한번에 dos에 변수 내용을 작성하고 `flush()` 하도록 함. (예외 발생의 가능성을 줄이기 위해)

--- src/main/java/webserver/protocol/HttpResponse.java
+++ src/main/java/webserver/protocol/HttpResponse.java
@@ -7,24 +7,44 @@ import java.io.DataOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.HashMap;
 import java.util.Map;
 
 public class HttpResponse {
-    private DataOutputStream dos;
+
+    private Logger logger = LoggerFactory.getLogger(HttpResponse.class);
+
+    private final DataOutputStream dos;
     private String responseLine;
-    private Map<String, String> headers;
+    private final Map<String, String> headers;
     private byte[] body;
 
     public HttpResponse(DataOutputStream dos) {
         this.dos = dos;
+        this.body = new byte[0];
+        this.headers = new HashMap<>();
+    }
+
+    public HttpResponse forwardStatic(String path) throws IOException {
+        try {
+            responseLine = "HTTP/1.1 200 OK";
+            body = Files.readAllBytes(new File("src/main/resources/static"+path).toPath());
+
+            setHeader("Content-Type", ContentType.of(path).value);
+            setHeader("Content-Length", String.valueOf(body.length));
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+        return this;
     }
 
     public HttpResponse forward(String path) throws IOException {
-        body = getClass().getResourceAsStream("/templates" + path).readAllBytes();
         try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html; charset=UTF-8 \r\n");
-            dos.writeBytes("Content-Length: " + body.length + "\r\n");
+            responseLine = "HTTP/1.1 200 OK";
+            body = Files.readAllBytes(new File("src/main/resources/templates"+path).toPath());
+
+            setHeader("Content-Type", ContentType.of(path).value);
+            setHeader("Content-Length", String.valueOf(body.length));
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
@@ -32,35 +52,46 @@ public class HttpResponse {
     }
 
     public HttpResponse redirect(String path) {
-        try {
-            dos.writeBytes("HTTP/1.1 302 Found \r\n");
-            dos.writeBytes("Location: "+path+"\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+        responseLine = "HTTP/1.1 302 Found";
+        setHeader("Location", path);
         return this;
     }
-
-        return this;
-    }
-
-    private HttpResponse setBody() {
-        try {
-            dos.writeBytes("\r\n");
-            dos.write(body, 0, body.length);
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-        return this;
-    }
-
 
     public void response() {
         try {
-            if (body.length > 0) setBody(); // 응답 하기 전, body가 있는 경우 자동으로 body를 뒤에 붙인다.
+            writeStatusCode();    // status 코드에 해당되는 ResponseLine을 작성한다.
+            writeHeader();  // 응답하기 전, head를 넣는다.
+
+            if (body.length > 0) {
+                writeBody();
+            }
 
             dos.writeBytes("\r\n");
             dos.flush();
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+
+    public HttpResponse setHeader(String key, String value) {
+        headers.put(key, value);
+        return this;
+    }
+
+    private void writeStatusCode() throws IOException {
+        dos.writeBytes(responseLine + " \r\n");
+    }
+
+    private void writeHeader() throws IOException {
+        for (Map.Entry<String, String> entries : headers.entrySet()) {
+            dos.writeBytes(String.format("%s: %s\r\n", entries.getKey(), entries.getValue()));
+        }
+    }
+
+    private void writeBody() {
+        try {
+            dos.writeBytes("\r\n");
+            dos.write(body, 0, body.length);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
