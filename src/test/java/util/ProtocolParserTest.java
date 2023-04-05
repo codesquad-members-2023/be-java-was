6a84cb7e02d7adfb679a8f4c85518ef41b8426db
saehim <saeilee.vxv@gmail.com>
2023-04-05 21:48:52 +0900

    Test (protocolParser) : parseHeader 기능 테스트

--- src/test/java/util/ProtocolParserTest.java
+++ src/test/java/util/ProtocolParserTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import webserver.protocol.HttpRequest;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -60,4 +62,24 @@ class ProtocolParserTest {
         assertThat(httpRequest.getParameter("email")).isEqualTo("asdf@naver");
     }
 
+    @Test
+    @DisplayName("parseHeader : 헤더 문자열이 주어졌을 때 키, 값으로 헤더를 파싱할 수 있다.")
+    void parseHeader() {
+        String example = "Host: www.example.com";
+        Map<String, String> parameter = ProtocolParser.parseHeaders(example);
+        assertThat(parameter.get("Host")).isEqualTo("www.example.com");
+    }
+
+    @Test
+    @DisplayName("parseHeader : 헤더가 6개인 헤더 문자열이 주어졌을 때 6개의 헤더를 모두 파싱할 수 있다.")
+    void parseHeaderCheckParseAll() {
+        String example = "Host: www.example.com\n" +
+                "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36\n" +
+                "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9\n" +
+                "Accept-Language: en-US,en;q=0.9,ko;q=0.8\n" +
+                "Connection: keep-alive\n" +
+                "Referer: https://www.google.com/";
+        Map<String, String> parameter = ProtocolParser.parseHeaders(example);
+        assertThat(parameter).hasSize(6);
+    }
 }
