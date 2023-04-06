5e0c0aa6c33c5786f2a352db92f759359f1102f9
acceptor-gyu <gseonggyu968@gmail.com>
2023-04-06 14:46:40 +0900

    알고보니 필요한 생성자 추가

--- src/main/java/util/SingletonContainer.java
+++ src/main/java/util/SingletonContainer.java
@@ -3,6 +3,11 @@ package util;
 import controller.UserController;
 
 public class SingletonContainer {
+    private UserController userController;
+
+    public SingletonContainer() {
+        this.userController = UserController.getInstance();
+    }
 
     public static UserController getUserController() {
         return UserController.getInstance();
