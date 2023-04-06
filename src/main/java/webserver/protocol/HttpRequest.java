81d22c2bcd838490a22d8eacfd2293ad175d44d4
saehim <saeilee.vxv@gmail.com>
2023-04-06 16:55:50 +0900

    Refactor : 시그니처 변경. 자바 컨벤션에 맞게 final 변수 대문자로 이름 변경

--- src/main/java/webserver/protocol/HttpRequest.java
+++ src/main/java/webserver/protocol/HttpRequest.java
@@ -6,41 +6,45 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class HttpRequest {
-    private final String method;
-    private final String path;
-    private final String httpVersion;
-    private final Map<String, String> queryParameter;
-    private final Map<String, String> headers;
+    private final String METHOD;
+    private final String PATH;
+    private final String HTTP_VERSION;
+    private final Map<String, String> QUERY_PARAMETERS;
+    private final Map<String, String> HEADERS;
 
     public HttpRequest(String requestLine, String headerStr) {
-        this.method = ProtocolParser.parseMethod(requestLine);
-        this.path = ProtocolParser.parsePath(requestLine);
-        this.httpVersion = ProtocolParser.parseVersion(requestLine);
-        this.queryParameter = ProtocolParser.parseQueryParameter(requestLine);
-        this.headers = ProtocolParser.parseHeaders(headerStr);
+        this.METHOD = ProtocolParser.parseMethod(requestLine);
+        this.PATH = ProtocolParser.parsePath(requestLine);
+        this.HTTP_VERSION = ProtocolParser.parseVersion(requestLine);
+        this.QUERY_PARAMETERS = ProtocolParser.parseQueryParameter(requestLine);
+        this.HEADERS = ProtocolParser.parseHeaders(headerStr);
     }
 
     public HttpRequest(String requestLine) {
-        this.method = ProtocolParser.parseMethod(requestLine);
-        this.path = ProtocolParser.parsePath(requestLine);
-        this.httpVersion = ProtocolParser.parseVersion(requestLine);
-        this.queryParameter = ProtocolParser.parseQueryParameter(requestLine);
-        this.headers = new HashMap<>();
+        this.METHOD = ProtocolParser.parseMethod(requestLine);
+        this.PATH = ProtocolParser.parsePath(requestLine);
+        this.HTTP_VERSION = ProtocolParser.parseVersion(requestLine);
+        this.QUERY_PARAMETERS = ProtocolParser.parseQueryParameter(requestLine);
+        this.HEADERS = new HashMap<>();
     }
 
-    public String getMethod() {
-        return method;
+    public String getMETHOD() {
+        return METHOD;
     }
 
-    public String getPath() {
-        return path;
+    public String getPATH() {
+        return PATH;
     }
 
     public String getParameter(String key) {
-        return queryParameter.get(key);
+        return QUERY_PARAMETERS.get(key);
     }
 
     public boolean isPath(String path) {
-        return this.path.equals(path);
+        return this.PATH.equals(path);
+    }
+
+    public String getVersion() {
+        return HTTP_VERSION;
     }
 }
