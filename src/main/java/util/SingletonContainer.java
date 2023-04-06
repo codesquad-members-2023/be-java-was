1a8b352cc5c51eb786f08359e72f29210e8ca8f2
acceptor-gyu <gseonggyu968@gmail.com>
2023-04-06 14:38:45 +0900

    불필요한 생성자 및 멤버 변수 삭제

--- src/main/java/util/SingletonContainer.java
+++ src/main/java/util/SingletonContainer.java
@@ -3,11 +3,6 @@ package util;
 import controller.UserController;
 
 public class SingletonContainer {
-    private UserController userController;
-
-    public SingletonContainer() {
-        this.userController = UserController.getInstance();
-    }
 
     public static UserController getUserController() {
         return UserController.getInstance();
