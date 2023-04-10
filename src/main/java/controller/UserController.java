5278bc8405dce83741d00e0b3639b2190d33dfe4
KOKEONHO <rhrjsgh97@gmail.com>
2023-04-10 10:52:14 +0900

    Feat: UserController.java에 UserSignUpService를 통한 회원가입 기능 추가
    UserSignUpService로 분리한 회원가입 서비스를 생성자를 통해 객체를 생성하여 saveUser() 메서드 기능 추가

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -2,13 +2,20 @@ package controller;
 
 import db.Database;
 import model.User;
+import service.UserSignUpService;
 
 import java.util.Collection;
 
 public class UserController {
 
-    public void saveUser(User user) {
-        Database.addUser(user);
+    private final UserSignUpService userSignUpService;
+
+    public UserController(UserSignUpService userSignUpService) {
+        this.userSignUpService = userSignUpService;
+    }
+
+    public void saveUser(String queryString) {
+        userSignUpService.userSignUp(queryString);
     }
 
     public Collection<User> showAllUsers() {
