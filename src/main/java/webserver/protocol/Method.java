eaed6a364ad3fcf2d2e46bda0a257bda077bcb7f
saehim <saeilee.vxv@gmail.com>
2023-04-10 17:08:13 +0900

    Feat (method) : post 추가, `of` 메서드 구현

--- src/main/java/webserver/protocol/Method.java
+++ src/main/java/webserver/protocol/Method.java
@@ -1,7 +1,15 @@
 package webserver.protocol;
 
+import java.util.Arrays;
+
 public enum Method {
-    GET;
+    GET, POST;
+
+    public static Method of(String s) {
+        return Arrays.stream(values()).filter(e -> e.name().equals(s))
+                .findAny()
+                .orElseThrow(() -> new RuntimeException("지원하지 않는 요청입니다."));
+    }
 
     public boolean equals(String methodname) {
         return name().equals(methodname);
