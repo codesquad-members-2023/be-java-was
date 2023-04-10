dd8d0ccfba5a935fdd5377ac611b23a165e6bc03
JeonHyoChang <jhc1426@naver.com>
2023-04-10 11:12:14 +0900

    ♻️refactor: 요구사항 반영(클래스 대문자 시작, enum 사용)

--- src/main/java/util/stylesheetUtils.java
+++ src/main/java/util/stylesheetUtils.java
@@ -1,23 +1,11 @@
 package util;
 
-public class stylesheetUtils {
+public class StylesheetUtils {
     public static String getContentType(String url) {
-        if (url.endsWith(".css")) {
-            return "text/css";
-        } else if (url.endsWith(".js")) {
-            return "application/javascript";
-        } else if (url.startsWith("/fonts")) {
-            return "application/octet-stream";
-        } else {
-            return "text/html";
-        }
+        return ContentType.of(url).getContentType();
     }
 
     public static String getPathName(String url) {
-        if (url.endsWith(".css") || url.endsWith(".js") || url.startsWith("/fonts")) {
-            return "src/main/resources/static";
-        } else {
-            return "src/main/resources/templates";
-        }
+        return ContentType.of(url).getPathName();
     }
 }
