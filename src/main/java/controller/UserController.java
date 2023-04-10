502a29c993add02988b037a33ceccfbadac5d9d7
KOKEONHO <rhrjsgh97@gmail.com>
2023-04-10 14:13:39 +0900

    Feat: UserController.java에서 웹 페이지 매핑하는 기능 구현
    HTTP 메서드가 GET일 경우 그에 맞는 웹 페이지 뷰를 매핑하는 기능 구현

--- src/main/java/controller/UserController.java
+++ src/main/java/controller/UserController.java
@@ -10,7 +10,27 @@ public class UserController {
         this.userSignUpService = userSignUpService;
     }
 
-    public void saveUser(String queryString) {
+    public String process(String httpMethod, String path, String uri) {
+        if (httpMethod.equals("GET")) {
+            if (path.equals("/user/form.html")) {
+                return "/user/form.html";
+            }
+            if (path.equals("/user/create")) {
+                String queryString = getQueryString(uri);
+                return saveUser(queryString);
+            }
+        }
+
+        return "/error";
+    }
+
+    private String getQueryString(String uri) {
+        String[] splittedUri = uri.split("\\?");
+        return splittedUri[1];
+    }
+
+    public String saveUser(String queryString) {
         userSignUpService.userSignUp(queryString);
+        return "/index.html";
     }
 }
