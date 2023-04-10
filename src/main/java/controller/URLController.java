0a275b46a46a3cd5e655d4a74e798820f49caec2
birdieHyun <flaehdan@gmail.com>
2023-04-10 15:16:34 +0900

    Feat : 성공적으로 실행 시 상태코드 200으로 설정

--- src/main/java/controller/URLController.java
+++ src/main/java/controller/URLController.java
@@ -1,25 +1,33 @@
 package controller;
 
 import config.AppConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import util.HttpRequest;
 import util.HttpResponse;
 
 public class URLController {
 
-    UserController userController = AppConfig.userController();
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private UserController userController = AppConfig.userController();
 
 
     public String mapUrl(String path, HttpRequest httpRequest, HttpResponse httpResponse) {
         // localhost:8080 기본화면으로 이동
         if (path.equals("/")) {
+            httpResponse.setStatus(200);
+            log.info("기본 화면으로 이동 ={}", path);
             return "/index.html";
         }
         // userController 호출
         if (path.startsWith("/user")) {
+            log.info("userController 호출 = {}", path);
             return userController.process(httpRequest, httpResponse);
         }
 
         // 기본 경로로 이동
+        log.info("들어온 경로 그대로 이동 = {}", path);
+        httpResponse.setStatus(200);
         return path;
     }
 }
