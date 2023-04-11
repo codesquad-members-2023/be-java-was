805ec6ca97b2d5c00a028b82b1c618d3c82021d6
KOKEONHO <rhrjsgh97@gmail.com>
2023-04-11 22:29:34 +0900

    Refactor: RequestParser.java 기존 INDEX였던 상수의 이름을 WELCOME_PAGE로 변경
    기존 상수 이름이었던 INDEX는 너무 직관적이지 않은 관계로 WELCOME_PAGE라는 이름으로 수정

--- src/main/java/request/RequestParser.java
+++ src/main/java/request/RequestParser.java
@@ -8,10 +8,15 @@ import java.util.Arrays;
 public class RequestParser {
 
     private static final Logger logger = LoggerFactory.getLogger(RequestParser.class);
+    private final String WELCOME_PAGE = "/index.html";
 
     public String[] parseRequestLine(String line) {
         String[] splitRequestLine = line.split(" ");
         logger.info(">> RequestParser -> splitRequestLine: {}", Arrays.toString(splitRequestLine));
+        // 만약 localhost:8080만 입력됐을 시
+        if (splitRequestLine[1].equals("/")) {
+            splitRequestLine[1] = WELCOME_PAGE;
+        }
         return splitRequestLine;
     }
 
