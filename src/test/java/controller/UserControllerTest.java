1324e15839f98996cb00659c17b1f063b6436cf1
saehim <saeilee.vxv@gmail.com>
2023-04-11 21:24:59 +0900

    Test (userController): 로그인 성공시 반환페이지 확인 테스트

--- src/test/java/controller/UserControllerTest.java
+++ src/test/java/controller/UserControllerTest.java
@@ -1,7 +1,6 @@
 package controller;
 
 import db.Database;
-import model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,14 +9,14 @@ import webserver.protocol.HttpRequest;
 import webserver.protocol.HttpResponse;
 import webserver.protocol.Method;
 
-import java.util.Collection;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.*;
 
 class UserControllerTest {
     @Mock
-    HttpRequest httpRequest;
+    HttpRequest httpRequestJoin;
+    @Mock
+    HttpRequest httpRequestLogin;
     @Mock
     HttpResponse httpResponse;
 
@@ -25,12 +24,13 @@ class UserControllerTest {
 
     @BeforeEach
     void init() {
-        httpRequest = mock(HttpRequest.class);
+        httpRequestJoin = mock(HttpRequest.class);
+        httpRequestLogin = mock(HttpRequest.class);
         httpResponse = mock(HttpResponse.class);
 
-        given(httpRequest.getMethod()).willReturn(Method.POST);
-        given(httpRequest.isPath("/user/create")).willReturn(true);
-        given(httpRequest.getBody()).willReturn("userId=member&password=1234&name=이린&email=iirin@naver.com");
+        given(httpRequestJoin.getMethod()).willReturn(Method.POST);
+        given(httpRequestJoin.isPath("/user/create")).willReturn(true);
+        given(httpRequestJoin.getBody()).willReturn("userId=member&password=1234&name=이린&email=iirin@naver.com");
         given(httpResponse.redirect("/")).willCallRealMethod();
 
         userController = new UserController();
@@ -39,25 +39,42 @@ class UserControllerTest {
     @Test
     @DisplayName("회원가입을 완료하면 redirect:/ String을 반환한다.")
     void joinTest() throws Exception{
-        String returnPage = userController.doPost(httpRequest, httpResponse);
+        String returnPage = userController.doPost(httpRequestJoin, httpResponse);
         assertThat(returnPage).isEqualTo("redirect:/");
     }
 
     @Test
     @DisplayName("회원 가입 후, 데이터 베이스에서 회원 아이디로 회원을 찾을 수 있다.")
     void joinAndGetUserIdTest() throws Exception{
-        userController.doPost(httpRequest, httpResponse);
+        userController.doPost(httpRequestJoin, httpResponse);
         assertThat(Database.findUserById("member")).isNotNull();
     }
 
     @Test
     @DisplayName("회원 가입 후, 데이터베이스 findAll 시 사이즈가 1 늘어나있다.")
     void joinAndGetSizeTest() throws Exception{
-        given(httpRequest.getBody()).willReturn("userId=Othermember&password=1234&name=이린&email=iirin@naver.com");
+        given(httpRequestJoin.getBody()).willReturn("userId=Othermember&password=1234&name=이린&email=iirin@naver.com");
 
         int beforeSize = Database.findAll().size();
-        userController.doPost(httpRequest, httpResponse);
+        userController.doPost(httpRequestJoin, httpResponse);
         assertThat(Database.findAll()).hasSize(beforeSize + 1);
+    }
+
+    @Test
+    @DisplayName("로그인 성공시 반환 페이지 테스트")
+    public void whenLoginFail() throws Exception{
+        //given
+        given(httpRequestJoin.getBody()).willReturn("userId=member&password=1234&name=이린&email=iirin@naver.com");
+        userController.doPost(httpRequestJoin, httpResponse);   // 회원가입한 상태
+
+        //when
+        given(httpRequestLogin.getMethod()).willReturn(Method.POST);
+        given(httpRequestLogin.isPath("/user/login")).willReturn(true);
+        given(httpRequestLogin.getBody()).willReturn("userId=member&password=1234");
+
+        //then
+        String returnPage = userController.doPost(httpRequestLogin, httpResponse);
+        assertThat(returnPage).isEqualTo("redirect:/");
     }
 
 }
