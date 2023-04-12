cdb89b338b5f7bd26c0b5f58cfa40b4e08d41a5c
KOKEONHO <rhrjsgh97@gmail.com>
2023-04-12 15:44:07 +0900

    Feat: RequestHandler.java Response 클래스 생성에 따른 메서드 분리 및 기존 메서드 주석 처리
    Request Header를 매핑한 headerMap에서 Accept 값을 확인하여 해당 Content Type으로 연결해주는 기능 추가

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -4,21 +4,25 @@ import java.io.*;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.Arrays;
+import java.util.Map;
 
 import config.AppConfig;
 import controller.UserController;
+import db.Database;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import request.Request;
+import request.RequestParser;
+import response.Response;
 
 public class RequestHandler implements Runnable {
 
-    private final String USER = "user";
-    private final String INDEX = "/index.html";
+    private final String USER_CREATE = "/user/create";
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
     private final Socket connection;
     private final AppConfig appConfig = new AppConfig();
     private final UserController userController = appConfig.makeUserController();
+    private RequestParser requestParser = new RequestParser();
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
@@ -30,72 +34,74 @@ public class RequestHandler implements Runnable {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            StringBuilder requestHeaderBuilder = new StringBuilder();       // Request Header
+            String requestLine = br.readLine();     // Request Line
+            logger.info(">> requestLine: {}", requestLine);
             String line = br.readLine();
-            if (line == null) {
-                return;
+            while (!line.equals("")) {
+                requestHeaderBuilder.append(line).append("\n");
+                line = br.readLine();
             }
-            logger.debug("request line: {}", line);
+            Map<String, String> headerMap = requestParser.parseRequestHeader(requestHeaderBuilder.toString());
+            logger.info(">> headerMap: {}", headerMap);
 
-            String[] splitRequestLine = line.split(" ");        // httpMethod / URI / httpVersion
-            logger.debug("splitRequestLine: {}", Arrays.toString(splitRequestLine));
-            String uri = getUri(splitRequestLine);
-            logger.debug("uri: {}", uri);
-            String path = getPath(uri);
-            logger.debug("path: {}", path);
-            String httpMethod = splitRequestLine[0];
+            String[] splitRequestLine = requestParser.parseRequestLine(requestLine);
+            Request request = createNewRequest(splitRequestLine);
 
-            String selectedController = findController(path);
+            logger.info(">> Request Handler -> request method: {}, request uri: {}, request version: {}", request.getMethod(), request.getUri(), request.getVersion());
 
-            if (selectedController.equals(INDEX)) {
-                path = INDEX;
-            }
-
-            if (selectedController.equals(USER)) {
-                path = userController.process(httpMethod, path, uri);
-                logger.debug("User: {}", path);
+            if (request.getUri().startsWith(USER_CREATE)) {
+                request.setUri(userController.saveUser(request.getUri()));
+                logger.info(">> Request Handler -> New User: {}", Database.findUserById("rhrjsgh97"));
             }
 
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(new File("src/main/resources/templates" + path).toPath());
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            String accept = headerMap.get("Accept").split(",")[0];
+            logger.info(">> RequestHandler -> accept: {}", accept);
+            logger.info(">> RequestHandler -> request.getUri(): {}", request.getUri());
+            byte[] body = null;
+            if (accept.endsWith("*") || accept.endsWith("css")) {
+                body = Files.readAllBytes(new File("src/main/resources/static" + request.getUri()).toPath());
+            } else {
+                body = Files.readAllBytes(new File("src/main/resources/templates" + request.getUri()).toPath());
+            }
+//            response200Header(dos, body.length);
+//            responseBody(dos, body);
+            sendResponse(dos, body, accept);
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+//    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+//        try {
+//            dos.writeBytes("HTTP/1.1 200 OK \r\n");
+//            dos.writeBytes("Content-Type: text/html; charset=utf-8\r\n");
+//            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+//            dos.writeBytes("\r\n");
+//        } catch (IOException e) {
+//            logger.error(e.getMessage());
+//        }
+//    }
+
+//    private void responseBody(DataOutputStream dos, byte[] body) {
+//        try {
+//            dos.write(body, 0, body.length);
+//            dos.flush();
+//        } catch (IOException e) {
+//            logger.error(e.getMessage());
+//        }
+//    }
+
+    private void sendResponse(DataOutputStream dataOutputStream, byte[] body, String accept) {
+        Response response = new Response(dataOutputStream, body.length, accept);
+        response.send200Header();
+        response.sendBody(body);
     }
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
+    private Request createNewRequest(String[] splitRequestLine) {
+        return new Request(splitRequestLine[0], splitRequestLine[1], splitRequestLine[2]);
     }
 
-    private String getUri(String[] splitRequestLine) {
-        return splitRequestLine[1];
-    }
-
-    private String getPath(String uriPath) {
-        String[] split = uriPath.split("\\?");
-        return split[0];
-    }
-
-    private String findController(String path) {
-        String[] split = path.split("/");
-        return split[1];
-    }
 }
