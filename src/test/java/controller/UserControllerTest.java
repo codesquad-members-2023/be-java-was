320f263df97b6f81cc1f4ad1b979ddd9e6ea38aa
saehim <saeilee.vxv@gmail.com>
2023-04-12 17:14:43 +0900

    Test (userController) : 로그인 실패시 테스트

--- src/test/java/controller/UserControllerTest.java
+++ src/test/java/controller/UserControllerTest.java
@@ -11,6 +11,8 @@ import webserver.protocol.Method;
 import webserver.protocol.StatusCode;
 import service.UserService;
 
+import java.io.IOException;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.*;
 
@@ -25,7 +27,7 @@ class UserControllerTest {
     UserController userController;
 
     @BeforeEach
-    void init() {
+    void init() throws IOException {
         httpRequestJoin = mock(HttpRequest.class);
         httpRequestLogin = mock(HttpRequest.class);
         httpResponse = mock(HttpResponse.class);
@@ -33,7 +35,11 @@ class UserControllerTest {
         given(httpRequestJoin.getMethod()).willReturn(Method.POST);
         given(httpRequestJoin.isPath("/user/create")).willReturn(true);
         given(httpRequestJoin.getBody()).willReturn("userId=member&password=1234&name=이린&email=iirin@naver.com");
-        given(httpResponse.redirect("/")).willCallRealMethod();
+
+        given(httpResponse.redirect(isA(String.class))).willReturn(httpResponse);
+        given(httpResponse.setCookie(isA(String.class), isA(String.class))).willReturn(httpResponse);
+        given(httpResponse.forward(isA(StatusCode.class), isA(String.class))).willReturn(httpResponse);
+        doNothing().when(httpResponse).response();
 
         userController = new UserController(new UserService());
     }
@@ -63,8 +69,24 @@ class UserControllerTest {
     }
 
     @Test
+    @DisplayName("회원 가입 시, 폼데이터가 없는 경우 반환페이지")
+    void joinEmptyFormTest() throws Exception{
+        given(httpRequestJoin.getBody()).willReturn("userId=&password=1234&name=이린&email=iirin@naver.com");
+        assertThat(userController.doPost(httpRequestJoin, httpResponse)).isEqualTo("/user/form_failed_empty.html");
+    }
+
+    @Test
+    @DisplayName("회원 가입 시, 유저 아이디가 중복되는 경우 반환페이지")
+    void joinDuplicateUserIdFormTest() throws Exception{
+        userController.doPost(httpRequestJoin, httpResponse);   // 첫번째 회원가입
+        given(httpRequestJoin.getBody()).willReturn("userId=member&password=1234111&name=이린2&email=iirin2@naver.com");
+
+        assertThat(userController.doPost(httpRequestJoin, httpResponse)).isEqualTo("/user/form_failed_duplicateUserId.html");
+    }
+
+    @Test
     @DisplayName("로그인 성공시 반환 페이지 테스트")
-    public void whenLoginFail() throws Exception{
+    void loginTest() throws Exception{
         //given
         given(httpRequestJoin.getBody()).willReturn("userId=member&password=1234&name=이린&email=iirin@naver.com");
         userController.doPost(httpRequestJoin, httpResponse);   // 회원가입한 상태
@@ -77,6 +99,40 @@ class UserControllerTest {
         //then
         String returnPage = userController.doPost(httpRequestLogin, httpResponse);
         assertThat(returnPage).isEqualTo("redirect:/");
+    }
+
+    @Test
+    @DisplayName("비밀번호가 달라 로그인이 실패했을 때, 반환 페이지 테스트")
+    void whenLoginFail() throws Exception{
+        //given
+        given(httpRequestJoin.getBody()).willReturn("userId=member&password=1234&name=이린&email=iirin@naver.com");
+        userController.doPost(httpRequestJoin, httpResponse);   // 회원가입한 상태
+
+        //when
+        given(httpRequestLogin.getMethod()).willReturn(Method.POST);
+        given(httpRequestLogin.isPath("/user/login")).willReturn(true);
+        given(httpRequestLogin.getBody()).willReturn("userId=member&password=1111");
+
+        //then
+        String returnPage = userController.doPost(httpRequestLogin, httpResponse);
+        assertThat(returnPage).isEqualTo("/user/login_failed.html");
+    }
+
+    @Test
+    @DisplayName("아이디가 DB에 없어 로그인이 실패했을 때, 반환 페이지 테스트")
+    void whenLoginFailCauseUserId() throws Exception{
+        //given
+        given(httpRequestJoin.getBody()).willReturn("userId=member&password=1234&name=이린&email=iirin@naver.com");
+        userController.doPost(httpRequestJoin, httpResponse);   // 회원가입한 상태
+
+        //when
+        given(httpRequestLogin.getMethod()).willReturn(Method.POST);
+        given(httpRequestLogin.isPath("/user/login")).willReturn(true);
+        given(httpRequestLogin.getBody()).willReturn("userId=member2&password=1234");
+
+        //then
+        String returnPage = userController.doPost(httpRequestLogin, httpResponse);
+        assertThat(returnPage).isEqualTo("/user/login_failed.html");
     }
 
 }
