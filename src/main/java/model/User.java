df3cd5c9c56160aaf0536639aaf70700a7e676ba
saehim <saeilee.vxv@gmail.com>
2023-04-12 21:25:41 +0900

    Update (user) : 생성자 파라미터에 이름 붙여줌

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,12 +1,17 @@
 package model;
 
+import annotation.Param;
+
 public class User {
     private String userId;
     private String password;
     private String name;
     private String email;
 
-    public User(String userId, String password, String name, String email) {
+    public User(@Param(name = "userId") String userId,
+                @Param(name = "password") String password,
+                @Param(name = "name") String name,
+                @Param(name = "email") String email) {
         this.userId = userId;
         this.password = password;
         this.name = name;
