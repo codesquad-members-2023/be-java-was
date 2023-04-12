1515df31236967d12b957578f39874678291e504
KOKEONHO <rhrjsgh97@gmail.com>
2023-04-12 13:10:16 +0900

    Feat: RequestParser.java HTTP Request의 Header 부분을 파싱하여 Map으로 반환하는 기능 추가
    String으로 들어온 requestHeader를 각각의 key와 value로 파싱하여 Map으로 반환하는 기능 추가

--- src/main/java/request/RequestParser.java
+++ src/main/java/request/RequestParser.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RequestParser {
 
@@ -13,11 +15,23 @@ public class RequestParser {
     public String[] parseRequestLine(String line) {
         String[] splitRequestLine = line.split(" ");
         logger.info(">> RequestParser -> splitRequestLine: {}", Arrays.toString(splitRequestLine));
-        // 만약 localhost:8080만 입력됐을 시
+        // 만약 localhost:8080만 입력됐을 시 -> WELCOME PAGE
         if (splitRequestLine[1].equals("/")) {
             splitRequestLine[1] = WELCOME_PAGE;
         }
         return splitRequestLine;
+    }
+
+    public Map<String, String> parseRequestHeader(String requestHeader) {
+        logger.info(">> RequestParser -> String requestHeader: {}", requestHeader);
+        String[] splitRequestHeader = requestHeader.split("\n");
+        logger.info("RequestParser -> splitRequestHeader: {}", Arrays.toString(splitRequestHeader));
+        Map<String, String> requestHeaderMap = new HashMap<>();
+        for (int i = 0; i < splitRequestHeader.length; i++) {
+            String[] headerKeyValue = splitRequestHeader[i].split(":", 2);
+            requestHeaderMap.put(headerKeyValue[0], headerKeyValue[1].trim());
+        }
+        return requestHeaderMap;
     }
 
 }
