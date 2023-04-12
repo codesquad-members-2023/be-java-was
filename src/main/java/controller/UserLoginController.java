b3fec09a6bbde436bf656177d3778bcfbdf79453
ngw7617@naver.com <ngw7617@naver.com>
2023-04-12 15:35:44 +0900

    feat(step-5):UserLoginController 구현, users/login url 매핑
    - 세션, 쿠키 기능 구현 예정
    - user/login -> users/login으로 변경

--- src/main/java/controller/UserLoginController.java
+++ src/main/java/controller/UserLoginController.java
@@ -1,2 +1,22 @@
-package controller;public class UserLoginController {
+package controller;
+
+import java.util.Map;
+
+import annotation.RequestMapping;
+import db.Database;
+import model.User;
+import request.HttpRequest;
+import request.HttpRequestUtils;
+import response.HttpResponse;
+
+@RequestMapping(url = "/users/login")
+public class UserLoginController implements Controller{
+    @Override
+    public String doPost(HttpRequest httpRequest, HttpResponse httpResponse) {
+        Map<String, String> params = HttpRequestUtils.parseQueryParams(httpRequest.getBody());
+
+        User user = Database.findUserById(params.get("userId")).get();
+
+        return "redirect:/";
+    }
 }
