987949f53ed4b161603c536f4c6ac6cf3c00fcbc
saehim <saeilee.vxv@gmail.com>
2023-04-12 14:29:41 +0900

    Feat : statusCode 추가
    - 401, 400

--- src/main/java/protocol/StatusCode.java
+++ src/main/java/protocol/StatusCode.java
@@ -3,7 +3,10 @@ package protocol;
 public enum StatusCode {
     OK(200, "200 OK"),
     FOUND(302, "302 Found"),
+    BAD_REQUEST(400, "400 Bad Request"),
     NOT_FOUND(404, "404 Not Found"),
+    UNAUTHORIZED(401, "401 Unauthorized");
+
     private int code;
     private String responseLine;
 
