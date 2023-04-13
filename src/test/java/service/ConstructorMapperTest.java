2bfa50640c6a4d33570ef16e30f483667fa615fe
birdieHyun <flaehdan@gmail.com>
2023-04-13 21:40:04 +0900

    Test : 로그인 유저 객체 생성 테스트

--- src/test/java/service/ConstructorMapperTest.java
+++ src/test/java/service/ConstructorMapperTest.java
@@ -1,5 +1,7 @@
 package service;
 
+import db.Database;
+import dto.user.UserLoginDTO;
 import model.User;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -18,5 +20,19 @@ class ConstructorMapperTest {
 
         // then
         Assertions.assertThat(user.getUserId()).isEqualTo("javajigi");
+    }
+
+    @Test
+    @DisplayName("올바르게 입력된 queryString을 통한 로그인 유저 객체 생성 테스트")
+    void makeLoginUserTest() {
+        // given
+        Database.addUser(new User("test", "test", "test", "test@email.com"));
+        String queryString = "userId=test&password=test";
+
+        // when
+        UserLoginDTO user = (UserLoginDTO) ConstructorMapper.makeConstructor(queryString, UserLoginDTO.class).orElseThrow(IllegalArgumentException::new);
+
+        // then
+        Assertions.assertThat(user.getUserId()).isEqualTo("test");
     }
 }
