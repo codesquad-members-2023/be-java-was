8ae824a3a3abcad226dc9f5fbf9bcf1d03c088ac
saehim <saeilee.vxv@gmail.com>
2023-04-13 14:19:06 +0900

    Update : 랜덤 id생성 로직 `UUID` 사용해서 변경

--- src/main/java/session/Session.java
+++ src/main/java/session/Session.java
@@ -1,6 +1,6 @@
 package session;
 
-import java.util.Random;
+import java.util.UUID;
 
 public class Session<T> {
 
@@ -29,10 +29,6 @@ public class Session<T> {
     }
 
     private String generateId() {
-        return new Random().ints('0', 'z'+1)
-                .filter(i -> ('0'<=i && i<='9') || ('A'<=i && i<='Z') || ('a'<=i && i<='z'))
-                .limit(ID_SIZE)
-                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
-                .toString();
+        return String.valueOf(UUID.randomUUID());
     }
 }
