980c51f21445e81b4abcc4ad602596b2c9d6e42f
birdieHyun <flaehdan@gmail.com>
2023-04-13 21:39:45 +0900

    Refactor : TODO 추가

--- src/main/java/service/UserLoginService.java
+++ src/main/java/service/UserLoginService.java
@@ -10,6 +10,8 @@ public class UserLoginService {
         UserLoginDTO loginUser = (UserLoginDTO) ConstructorMapper.makeConstructor(loginInformation, UserLoginDTO.class)
                 .orElseThrow(IllegalArgumentException::new);
 
+        // TODO : null 로 반환하는 것이 아닌, 예외처리 필요
+        // 비밀번호 조회 후 같으면 유저를 반환, 아닐 경우 null을 반환
         return Database.findUserById(loginUser.getUserId())
                 .filter(m -> m.getPassword().equals(loginUser.getPassword()))
                 .orElse(null);
