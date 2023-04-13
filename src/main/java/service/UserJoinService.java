1e55d5d515319e4b649a939ae51d81e78ed4f96d
birdieHyun <flaehdan@gmail.com>
2023-04-13 21:24:46 +0900

    Refactor : 사용되지 않는 import 제거

--- src/main/java/service/UserJoinService.java
+++ src/main/java/service/UserJoinService.java
@@ -1,13 +1,9 @@
 package service;
 
-import cookie.Cookie;
 import db.Database;
-import dto.user.UserLoginDTO;
 import model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import static java.util.UUID.*;
 
 public class UserJoinService {
     private final Logger log = LoggerFactory.getLogger(getClass());
