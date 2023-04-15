73bc9dc61ff0b14a17075bc39263a5ddcc629afe
saehim <saeilee.vxv@gmail.com>
2023-04-15 15:47:28 +0900

    Refactor : 공통으로 사용되는 상수 추상클래스 구현

---
+++ src/main/java/util/Constants.java
@@ -0,0 +1,9 @@
+package util;
+
+public interface Constants {
+    public static final String NEW_LINE = System.lineSeparator();
+    public static final String DOMAIN = "localhost:8080";
+    public static final String BASE_PATH = "/";
+    public static final String BASE_PAGE = "/index.html";
+
+}
