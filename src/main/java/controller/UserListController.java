5db58de045228bfff324579ae8c9897c3d6fef06
ngw7617@naver.com <ngw7617@naver.com>
2023-04-17 19:40:17 +0900

    feat(step-6):로그인이 안되어있으면 UserList를 보지 못하고 login창으로 redirect되도록 기능 구현

--- src/main/java/controller/UserListController.java
+++ src/main/java/controller/UserListController.java
@@ -1,16 +1,32 @@
 package controller;
 
+import java.util.Optional;
+
 import annotation.MethodType;
 import annotation.RequestMapping;
 import db.Database;
+import model.User;
 import request.HttpRequest;
+import request.HttpRequestUtils;
 import response.HttpResponse;
+import session.SessionDb;
 
 @RequestMapping(url = "/users/list")
 public class UserListController extends Controller {
     @MethodType(value = "GET")
     public String userList(HttpRequest httpRequest, HttpResponse httpResponse) {
-        //유저 목록 조회
+        Optional<String> sessionId = httpRequest.getSessionId();
+        if (!sessionId.isEmpty()) {
+            String parsedSessionId = HttpRequestUtils.parseSessionId(sessionId.get());
+
+            if (SessionDb.getUserBySessionId(parsedSessionId) == null) {
+                return "redirect:/user/login.html";
+            }
+        }
+        else {
+            return "redirect:/user/login.html";
+        }
+            //유저 목록 조회
         if (!Database.getUserList().isEmpty()) {
             httpResponse.setModelAttribute("users", Database.getUserList());
         }
