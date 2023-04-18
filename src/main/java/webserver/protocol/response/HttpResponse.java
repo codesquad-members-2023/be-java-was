a80dea61c8ca524a5ca488b14628dbd302b6e1ce
saehim <saeilee.vxv@gmail.com>
2023-04-18 20:32:51 +0900

    Refactor (response) : httpResponse 일급컬렉션으로 수정

---
+++ src/main/java/webserver/protocol/response/HttpResponse.java
@@ -0,0 +1,77 @@
+package webserver.protocol.response;
+
+import com.google.common.net.HttpHeaders;
+import webserver.protocol.ContentType;
+
+import java.nio.charset.StandardCharsets;
+
+import static util.Constants.*;
+
+public class HttpResponse {
+    public static final String REDIRECT_KEY = "redirect:";
+    private final String HTTP_VERSION = "HTTP/1.1";
+    private StatusCode statusCode;
+    private HttpResponseHeader httpResponseHeader;
+    private String path;
+    private byte[] body;
+
+    private HttpResponse(StatusCode statusCode, HttpResponseHeader httpResponseHeader, String path, byte[] body) {
+        this.statusCode = statusCode;
+        this.httpResponseHeader = httpResponseHeader;
+        this.path = path;
+        this.body = body;
+    }
+
+    public void sendForward(byte[] body, ContentType viewType) {
+        if (statusCode.equals(StatusCode.ACCEPTED)) {
+            statusCode = StatusCode.OK;
+        }
+        this.body = body;
+        httpResponseHeader.forward(viewType, body.length);
+    }
+
+    public void sendRedirect(String viewPath) {
+        path = path.replace(REDIRECT_KEY, EMPTY);
+        statusCode = StatusCode.FOUND;
+        httpResponseHeader.redirect(path);
+    }
+
+    public HttpResponse setStatus(StatusCode newStateCode) {
+        statusCode = newStateCode;
+        return this;
+    }
+
+    public HttpResponse setCookie(String key, String value) {
+        httpResponseHeader.setCookie(key, value);
+        return this;
+    }
+
+    public String getCookie(){
+        return httpResponseHeader.getHeader(HttpHeaders.SET_COOKIE);
+    }
+
+    /**
+     * @return Response Message를 String으로 반환한다.
+     */
+    public String getMessage() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(HTTP_VERSION).append(SPACE);
+        sb.append(statusCode.toString()).append(NEW_LINE);
+
+        sb.append(httpResponseHeader.toString());
+
+        if (body.length >0) {
+            sb.append(new String(body, StandardCharsets.UTF_8));
+        }
+
+        return sb.toString();
+    }
+
+    /**
+     * @return Accepted 코드를 가진 response 새로운 객체
+     */
+    public static HttpResponse accepted () {
+        HttpResponseHeader httpResponseHeader = HttpResponseHeader.create();
+        return new HttpResponse(StatusCode.ACCEPTED, httpResponseHeader, BASE_PATH, new byte[0]);
+    }
+}
---
+++ src/main/java/webserver/protocol/response/HttpResponseHeader.java
@@ -0,0 +1,82 @@
+package webserver.protocol.response;
+
+import com.google.common.net.HttpHeaders;
+import webserver.protocol.ContentType;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static util.Constants.*;
+
+public class HttpResponseHeader {
+    private final String SEPARATOR = ": ";
+    private Map<String, String> headers;
+    private Map<String, Cookie> cookies;
+
+    public HttpResponseHeader(Map<String, String> headers, Map<String, Cookie> cookies) {
+        this.headers = headers;
+        this.cookies = cookies;
+    }
+
+    private void setHeader(String headerKey, String value) {
+        headers.put(headerKey, value);
+    }
+
+    public String getHeader(String headerKey) {
+        return headers.get(headerKey);
+    }
+
+    public void setCookie (String cookieKey, String value) {
+        Cookie cookie = new Cookie(cookieKey, value);
+        cookie.setDomain(DOMAIN);
+        cookie.setPath(BASE_PATH);
+        cookies.put(cookieKey, cookie);
+    }
+
+    public String getSetCookie () {
+        return headers.get(HttpHeaders.SET_COOKIE);
+    }
+
+    public String getCookie(String key) {
+        return cookies.get(key).toString();
+    }
+
+    public void forward(ContentType type, int length) {
+        headers.put(HttpHeaders.CONTENT_TYPE, type.getHeadValue());
+        headers.put(HttpHeaders.CONTENT_LENGTH, String.valueOf(length));
+    }
+
+    public void redirect(String path) {
+        headers.put(HttpHeaders.LOCATION, path);
+    }
+
+    public static HttpResponseHeader create() {
+        return new HttpResponseHeader(new HashMap<>(), new HashMap<>());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, String> entry : headers.entrySet()) {
+            sb.append(entry.getKey()).append(SEPARATOR).append(entry.getValue()).append(NEW_LINE);
+        }
+
+        if (!cookies.isEmpty()) {
+            sb.append(writeCookie());
+        }
+        sb.append(NEW_LINE);
+
+        return sb.toString();
+    }
+
+    private String writeCookie() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(HttpHeaders.SET_COOKIE).append(SEPARATOR);
+
+        for (Map.Entry<String, Cookie> entry : cookies.entrySet()) {
+            sb.append(entry.getValue().toString()).append(SPACE);
+        }
+
+        return sb.toString().trim();
+    }
+}
