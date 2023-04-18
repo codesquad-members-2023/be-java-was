4ad5476eee83bcd9e3dec782d0f1516c7a39d284
saehim <saeilee.vxv@gmail.com>
2023-04-18 20:32:18 +0900

    Refactor : 시그니처 변경

--- src/main/java/webserver/protocol/ContentType.java
+++ src/main/java/webserver/protocol/ContentType.java
@@ -5,23 +5,24 @@ import java.util.Arrays;
 public enum ContentType {
 
     JS("application/javascript", ".js", "/static"),
-    CSS("text/css;charset=utf-8", ".css", "/static"),
-    FONT("font/woff", ".woff", "/static"),
+    CSS("text/css", ".css", "/static"),
+    FONT_WOFF("font/woff", ".woff", "/static"),
+    FONT_TTF("font/ttf", ".ttf", "/static"),
     PNG("image/png", ".png", "/static"),
     ICO("image/avif", ".ico", "/templates"),
-    HTML("text/html;charset=utf-8", ".html", "/templates");
+    HTML("text/html", ".html", "/templates");
 
+    private final String RESOURCE_PATH = "src/main/resources";
     private String headValue;
     private String pattern;
     private String typePath;
-    private final String CLASS_PATH = "src/main/resources";
 
     public String getHeadValue() {
         return headValue;
     }
 
     public String getTypeDirectory() {
-        return CLASS_PATH + typePath;
+        return RESOURCE_PATH + typePath;
     }
 
     ContentType(String headValue, String pattern, String typePath) {
