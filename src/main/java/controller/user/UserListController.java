c8792ff835aacdce12ccdfe064eec06b8b6e5262
saehim <saeilee.vxv@gmail.com>
2023-04-19 15:07:53 +0900

    Feat (ListController) : 권한 확인 추가

--- src/main/java/controller/user/UserListController.java
+++ src/main/java/controller/user/UserListController.java
@@ -8,6 +8,9 @@ import webserver.protocol.request.HttpRequest;
 import webserver.protocol.response.HttpResponse;
 
 import java.io.IOException;
+import java.nio.file.AccessDeniedException;
+
+import static webserver.RequestHandler.logger;
 
 public class UserListController extends FrontController {
 
@@ -19,6 +22,12 @@ public class UserListController extends FrontController {
 
     @Override
     protected String doGet(HttpRequest httpRequest, HttpResponse httpResponse, Model model) throws IOException {
+        logger.debug("sessionKey = {}", httpRequest.getSessionKey());
+        logger.debug("isSessionValid = {}", String.valueOf(httpRequest.isSessionValid()));
+        if (!httpRequest.isSessionValid()) {
+            throw new AccessDeniedException("로그인 후 이용 가능합니다.");
+        }
+
         model.addObject("user", Database.findAll());
         return "/user/list.html";
     }
