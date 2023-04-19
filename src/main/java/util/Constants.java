9d9cc5096c23ef8774e3338396d67a02627cffa8
saehim <saeilee.vxv@gmail.com>
2023-04-19 15:08:29 +0900

    Fix : domain 시그니처 수정

--- src/main/java/util/Constants.java
+++ src/main/java/util/Constants.java
@@ -2,9 +2,8 @@ package util;
 
 public abstract class Constants {
     public static final String NEW_LINE = System.lineSeparator();
-    public static final String DOMAIN = "localhost:8080";
+    public static final String DOMAIN = "localhost";
     public static final String BASE_PATH = "/";
-    public static final String BASE_PAGE = "/index.html";
     public static final String EQUALS = "=";
     public static final String SPACE = " ";
     public static final String EMPTY = "";
